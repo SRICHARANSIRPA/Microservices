62eddbdfc81be9f97b0cbf48c8d9658f07e9fb8b
SRICHARANSIRPA <sricharansirpa@gmail.com>
2021-07-04 16:35:05 +0530

    Changes in BasketAPI Microservice

---
+++ src/Services/Basket/Basket.API/Configuration/ConfigurationValues.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Configuration
+{
+    public class ConfigurationValues
+    {
+        public CacheSettings CacheSettings;
+    }
+
+    public class CacheSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,52 @@
+﻿using Basket.API.Configuration;
+using Basket.API.Entities;
+using Basket.API.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository basketRepository;
+        private readonly ConfigurationValues configurationValues;
+
+
+        public BasketController(IBasketRepository basketRepository, IOptions<ConfigurationValues> configurationValues)
+        {
+            this.configurationValues = configurationValues.Value;
+            this.basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+        }
+
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await basketRepository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await basketRepository.UpdateBasket(basket));
+        }
+
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> DeleteBasket(string userName)
+        {
+            await basketRepository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
