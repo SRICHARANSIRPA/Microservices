426006754eb87831f6d7883d325f8cdda40961f1
SRICHARANSIRPA <sricharansirpa@gmail.com>
2021-07-25 02:14:07 +0530

    Added Docker Support to the Discount API and also developed the Discount Grpc Services

---
+++ src/Services/Discount/Discount.Grpc/Entities/Coupon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string productName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,20 @@
+﻿using Discount.Grpc.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string ProductName);
+
+        public Task<bool> CreateDiscount(Coupon coupon);
+
+        public Task<bool> UpdateDiscount(Coupon coupon);
+
+        public Task<bool> DeleteDiscount(string productName);
+
+    }
+}
